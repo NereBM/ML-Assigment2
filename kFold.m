cf62039988dabdd81328af75f723461eee016cec
psyrla <psyrla@nottingham.ac.uk>
2018-11-08 17:53:31 +0000

    Fixed kFold function.

--- kFold.m
+++ kFold.m
@@ -5,10 +5,36 @@ function data = kFold(features, labels)
     cv = cvpartition(size(features, 1), 'kfold', k);
     
     for i = 1: cv.NumTestSets
-        root = createTree(transpose(features(cv.training(i), :)), ...
+        % Create tree from training data.
+        tree = createTree(transpose(features(cv.training(i), :)), ...
             labels(cv.training(i)));
-        DrawDecisionTree(root);
-        pause
+        
+        % Classify test data using tree.
+        predicted = classify(features(cv.test(i), :), tree);
+        
+        % Calculate F1 score from labels(cv.test(i)) and predicted.
+        
+        % DrawDecisionTree(root);
+        % pause
+        
     end
+end
+
+
+function predicted = classify(features, tree)
     
+    predicted = transpose(zeros(size(features, 1), 1));
+    for i = 1: size(features, 1)
+        node = tree;
+        x = (features(i, :));
+        % Traverse tree until leaf node.
+        while ~isempty(node.kids)
+            if x(node.attribute) < node.threshold
+                node = node.kids{1};
+            else
+                node = node.kids{2};
+            end
+        end
+        predicted(i) = node.class;
+    end
 end
